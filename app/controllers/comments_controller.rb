424ab5d9ba0b811b867aa2c083b43ae1901ec8a4
manthon0 <manthon0@chicagobooth.edu>
2023-01-25 01:47:54 +0000

    stopped users from editing deleting others comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,14 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[ show edit update destroy ]
 
+  before_action :ensure_current_user_is_author, only: [:edit, :update, :destroy]
+  
+  def ensure_current_user_is_author
+    if current_user != @comment.author
+      redirect_back(fallback_location: root_url, notice: "nah")
+    end
+  end
+
   # GET /comments or /comments.json
   def index
     @comments = Comment.all
